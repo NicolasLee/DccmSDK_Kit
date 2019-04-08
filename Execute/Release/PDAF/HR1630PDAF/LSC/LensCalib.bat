1d11d185641fa837a75a540f06c5861a82c6bfa8
Nicolas Lee <312725531@qq.com>
2019-04-08 10:35:29 +0800

    delete .bak

--- Execute/Release/PDAF/HR1630PDAF/LSC/LensCalib.bat
+++ Execute/Release/PDAF/HR1630PDAF/LSC/LensCalib.bat
@@ -1,1 +1,1 @@
-LensCalib.exe -input dnp.raw -falloff 65 -outini lsc_and_pdaf_otpm.ini -outiniformat OTPM -whitepoint wp.ini -for AR1630 -rev 2
+LensCalib.exe -input 1.raw -falloff 65 -outini lsc_and_pdaf_otpm.ini -outiniformat OTPM -whitepoint wp.ini -for AR1630 -rev 2
